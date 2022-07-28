9a4ece94be170c3f6b53052098b47dcc9dd6121f
LuizFelipe1F <108068294+LuizFelipe1F@users.noreply.github.com>
2022-07-28 08:15:15 -0300

    Create Jogadores.js

---
+++ Jogadores.js
@@ -0,0 +1,39 @@
+function Player(){
+  this.w = 15;
+  this.h = 80;
+  this.pos = createVector(this.w*2, height/2-this.h/2);
+  this.acc = createVector(0,0);
+  this.spd = 10;
+  this.maxSpd = 10;
+
+  this.show = function(){
+    noStroke();
+    fill(255);
+    rect(this.pos.x, this.pos.y, this.w, this.h);
+  }
+
+  this.up = function(){
+    this.acc.y-=this.spd;
+  }
+
+  this.down = function(){
+    this.acc.y+=this.spd;
+  }
+
+  this.move = function(multiplier){
+    this.acc.y+=this.spd*multiplier;
+  }
+
+  this.stp = function(){
+    this.acc.y = 0;
+  }
+
+  this.update = function(){
+    this.acc.y = constrain(this.acc.y, -this.maxSpd, this.maxSpd);
+    this.pos.add(this.acc);
+    this.acc.y *= 0.89;
+    this.pos.y = constrain(this.pos.y, 0, height-this.h);
+
+    //console.log(this.acc.y);
+  }
+}
